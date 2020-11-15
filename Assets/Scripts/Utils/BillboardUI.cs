7f41de5ccb7933ff37ab084d5659f12e55be86aa
Ivan Castellano <justdarsan@gmail.com>
2020-11-14 22:01:17 -0300

    Added basic assets and basic UI

---
+++ Assets/Scripts/Utils/BillboardUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardUI : MonoBehaviour
+{
+    Quaternion originalRotation;
+    void Start()
+    {
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation * originalRotation;
+    }
+}
